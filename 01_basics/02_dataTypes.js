77b9a7b8550bea61db11b65328a912315d6c65a5
Nishh2303 <153318941+Nishh2303@users.noreply.github.com>
2025-02-15 22:38:31 +0000

    Datatypes done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+console.log("Nisarg");
+
+
+let name = "Nisarg"
+let age = 18
+let isLoggedIn = false
+let state;
+let temprature = null // 0 is value and null means empty
+
+// number => 2 to power 53
+// bigint ( used for big big numbers like in trading etc)
+// string => " "
+// boolean => true/false
+// null => (it means empty)standalone value
+// undefined 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
